332fa32c49f17248b47d203331b0595f435852ee
Gukapriyan <88232274+Gukapriyan@users.noreply.github.com>
2022-05-12 22:35:02 +0530

    Add files via upload

---
+++ MyTowWheller.java
@@ -0,0 +1,12 @@
+package assignment1;
+
+public class MyTowWheller {
+
+	public static void main(String[] args) {
+		TowWheeler object = new TowWheeler();
+		String bikeName= object.bikeName;
+		System.out.println(bikeName);
+		
+	} 
+
+}
---
+++ TowWheeler.java
@@ -0,0 +1,18 @@
+package assignment1;
+
+public class TowWheeler {
+	int noOfWheels = 2;
+	short noOfMirrors = 2;
+	long chassisNumber = 65784378244l;
+	boolean isPurnctured = true;
+	String bikeName = "Yamaha";
+	double ruinningKm = 45000.005;
+	public static void main(String[] args) {
+		TowWheeler object = new TowWheeler();
+		
+		System.out.println(object.bikeName);
+		
+
+	}
+
+}
